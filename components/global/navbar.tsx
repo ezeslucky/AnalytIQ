71838433b8abd90b7cda4be2738626c10ffc147e
Lucky <lucky123@gmail.com>
2025-03-14 19:51:18 +0530

    navbar

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -1,10 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
+import { Button } from '../ui/button'
+import { Menu } from 'lucide-react'
+import Sidebar from './sidebar'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav className=' top-0 left-0 z-[3] fixed flex justify-between items-center bg-black backdrop-blur-md px-6 pt-3  border-gray-800 border-b w-full '>
+<Link href="/" className=' flex items-center gap-2' >
+<Image src="/logo.svg" width={28} height={28} alt='logo'/>
+<span className=' bg-clip-text bg-gradient-to-r from-white to-neutral-500 font-medium text-transparent text-xl'>
+AnalytIQ
+</span>
+</Link>
+<div className=" flex items-center gap-2">
+  <Sheet>
+    <SheetTrigger asChild>
+<Button size="icon" className=' md:hidden bg-[#C05D5D]  hover:bg-[rgba(192,93,93,0.8)] text-gray-300 '>
+<Menu size={24}/>
+<span className=" sr-only"> Toggle Menu</span>
+
+</Button>
+    </SheetTrigger>
+    <SheetContent className=' bg-black p-0 border-gray-800 border-r w-64 ' side='left'>
+<Sidebar/>
+    </SheetContent>
+  </Sheet>
+</div>
+    </nav>
   )
 }
 
