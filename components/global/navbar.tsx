c9441095c2da5abc709b52416146d192d543d455
Lucky <lucky123@gmail.com>
2025-03-14 19:05:05 +0530

    navbar

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
-        
+    <nav className=' top-0 left-0 z-[3] fixed flex justify-between items-center bg-black backdrop-blur-md px-6 pt-3 pb-3 border-gray-800 border-b w-full'>
+
     </nav>
   )
 }
