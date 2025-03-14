0b82e7e6bd69d0cc7d09690409daa39b6d033963
Lucky <lucky123@gmail.com>
2025-03-15 00:23:43 +0530

    sidebar-context

--- components/global/sidebar.tsx
+++ components/global/sidebar.tsx
@@ -1,9 +1,11 @@
  "use client"
  import React from 'react'
+import { useSidebar } from '../../contexts/sidebar-context'
 
 
 function Sidebar() {
   const {isCollapsed, toogleSidebar} = useSidebar()
+  
   return (
     <aside className=''>
 
--- contexts/sidebar-context.tsx
+++ contexts/sidebar-context.tsx
@@ -1,7 +1,60 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 'use client'
 
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 
 
-import React from 'react'
+type SidebarContextType ={
+    isCollapsed: boolean;
+    toogleSidebar: () =>  void
+}
+
+const SidebarContext = createContext<SidebarContextType | undefined>
+(undefined)
+
+
+export const SidebarProvider = ({
+    children,
+}:{ 
+    children: React.ReactNode;
+}) => {
+    const [isCollapsed, setIsCollapsed] = useState(false)
+
+    const toggleSidebar = useCallback(()=>{
+        setIsCollapsed((prev) => !prev)
+    }, [])
+
+    useEffect(() =>{
+        const handleKeyDown = (event:KeyboardEvent) => {
+            if(
+                (event.metaKey && event.key === "b") || (event.ctrlKey && event.key === "b")
+            ){
+                event.preventDefault()
+                toggleSidebar()
+            }
+        }
+
+        window.addEventListener("keydown", handleKeyDown)
+
+        return()=>{
+            window.removeEventListener('keydown', handleKeyDown)
+        }
+    },[toggleSidebar])
+
+    return (
+        //@ts-ignore
+        <SidebarContext.Provider value={{ isCollapsed,toggleSidebar}}>
+            {children}
+        </SidebarContext.Provider>
+    )
+}
+
+export const useSidebar = () =>{
+    const context = useContext(SidebarContext)
+    if(context === undefined){
+        throw new Error("use In SidebarProvider")
+    }
+    return context
+}
 
  
